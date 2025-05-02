400874ae06898cce89a4f3fb4bd49b170fc86b2a
nakshayan <animalat@uwaterloo.ca>
2025-05-02 04:59:29 -0400

    fetch in frontend

--- merchant-lens/src/lib/api.ts
+++ merchant-lens/src/lib/api.ts
@@ -11,3 +11,51 @@ export async function fetchOverview(merchantId: number) {
 
     return res.json();
 }
+
+export async function fetchMonthOverview(merchantId: number) {
+    const res = await fetch(`${baseUrl}/api/metrics/month-overview?merchantId=${merchantId}`, {
+        cache: "no-store",
+    });
+    
+    if (!res.ok) {
+        throw new Error("Failed to fetch metrics");
+    }
+
+    return res.json();
+}
+
+export async function fetchCustomerMetrics(merchantId: number, month: string) {
+    const res = await fetch(`${baseUrl}/api/metrics/customer-metrics?merchantId=${merchantId}&month=${month}`, {
+        cache: "no-store",
+    });
+    
+    if (!res.ok) {
+        throw new Error("Failed to fetch metrics");
+    }
+
+    return res.json();
+}
+
+export async function fetchTopProducts(merchantId: number, month: string) {
+    const res = await fetch(`${baseUrl}/api/metrics/top-products?merchantId=${merchantId}&month=${month}`, {
+        cache: "no-store",
+    });
+    
+    if (!res.ok) {
+        throw new Error("Failed to fetch metrics");
+    }
+
+    return res.json();
+}
+
+export async function fetchRevenueOverTime(merchantId: number, numTime: number, timeType: string) {
+    const res = await fetch(`${baseUrl}/api/metrics/revenue-over-time?merchantId=${merchantId}&numTime=${numTime}&timeType=${timeType}`, {
+        cache: "no-store",
+    });
+    
+    if (!res.ok) {
+        throw new Error("Failed to fetch metrics");
+    }
+
+    return res.json();
+}
